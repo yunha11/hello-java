64dd1703c24c8ea0ff84f478eb148b95c4524bfe
yunha <alr0604@gmail.com>
2019-05-21 21:32:30 +0900

    입력받은 달의 일수 출력

--- src/calender/Month.java
+++ src/calender/Month.java
@@ -8,19 +8,23 @@ public class Month {
 	{
 		int[] month=new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
 		
-		System.out.println("달을 입력하세요. ");
-		
+		System.out.println("반복횟수를 입력하세요. ");
 		Scanner sc = new Scanner(System.in);
-		int mon = sc.nextInt();
+		int t = sc.nextInt();
 		
-		try
+		for(int i=0;i<t;i++)
 		{
-			System.out.println(mon+"월은 "+month[mon-1]+"일까지 있습니다.");
-		}catch(Exception e)
-		{
+			System.out.println("달을 입력하세요. ");
 			
+			
+			int mon = sc.nextInt();
+			
+			try {
+				System.out.println(mon + "월은 " + month[mon - 1] + "일까지 있습니다.");
+			} catch (Exception e) {
+
+			}
 		}
-		
-		
+		sc.close();
 	}
 }
