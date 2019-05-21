cb35313227ff6aabc42a9a3df53b21f9e06e31e3
yunha <alr0604@gmail.com>
2019-05-21 17:42:16 +0900

    메인클래스를 따로 만들어 적용

---
+++ src/hello_java/GugudanMain.java
@@ -0,0 +1,14 @@
+package hello_java;
+
+public class GugudanMain {
+
+	public static void main(String args[])
+	{
+		for(int i=2;i<=9;i++)
+		{
+			int[] result = Gugudan03.gugu(i);
+			Gugudan03.view(result);
+		}
+		
+	}
+}
