4aa7d8d46bfb516d93de35512fc62b29d5a516a3
yunha <alr0604@gmail.com>
2019-05-24 16:40:19 +0900

    반복입력 받기

--- src/calender/CalenderMain.java
+++ src/calender/CalenderMain.java
@@ -1,15 +1,28 @@
 package calender;
 
+import java.util.Scanner;
+
 public class CalenderMain {
 
+	public static int[] month = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
+	
 	public static void main(String args[])
 	{
-		System.out.println("일   월   화   수   목   금   토");
-		System.out.println("--------------------");
-		System.out.println("1  2  3  4  5  6  7");
-		System.out.println("8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+		Scanner scanner = new Scanner(System.in);
 		
+		while(true)
+		{
+			System.out.println("월을 입력하세요.");
+			System.out.print(">");
+			int mon = scanner.nextInt();
+			if(mon==-1) break;
+	
+			System.out.printf("%d월은 %d일까지 있습니다.\n", mon, month[mon-1]);
+			
+			
+			
+		}
+		
+		scanner.close();
 	}
 }
