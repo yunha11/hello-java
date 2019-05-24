a6b1a1274e1c75728f9276c09dcce477a9e472da
yunha <alr0604@gmail.com>
2019-05-24 18:10:41 +0900

    달력 구현 완료

--- src/calender/Calendar.java
+++ src/calender/Calendar.java
@@ -6,29 +6,95 @@ public class Calendar {
 
 	private static final int[] month = new int[] {31,28,31,30,31,30,31,31,30,31,30,31};
 	private static final int[] leaf_month = new int[] {31,29,31,30,31,30,31,31,30,31,30,31};
+		
+	public boolean isLeapYear(int y) {
+		if (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0))
+			return true;
+		else
+			return false;
+	}
 	
 	public int getDays(int y, int m)
 	{
-		if(y%4==0) return leaf_month[m-1];
+		if(isLeapYear(y)) return leaf_month[m-1];
 		else return month[m-1];
 	}
 	
-	public void printCalendar(int y, int m)
+	public void printCalendar(int y, int m, int w)
 	{
 		System.out.println("\t<<%4d년 %2d월>>");
 		System.out.println("일   월   화   수   목   금   토");
-		System.out.println("--------------------");
+		System.out.println("----------------------");
 		
 		int maxDay = getDays(y, m);
 		
+		for(int i=0; i<w; i++)
+			System.out.print("   ");
+		
 		for(int i=1; i<= maxDay; i++)
 		{
-			if(i%7==0)
+			if((i+w)%7==0)
 				System.out.printf(" %2d\n", i);
 			else
 				System.out.printf(" %2d", i);
 		}
 		
 		System.out.printf("\n");
+	}
+	
+	public void printCalendar(int y, int m)
+	{
+		System.out.println("\t<<%4d년 %2d월>>");
+		System.out.println("일   월   화   수   목   금   토");
+		System.out.println("----------------------");
+		
+		int weekday = getWeekDay(y, m, 1); 
+		
+		for(int i=0; i<weekday; i++)
+			System.out.print("   ");
+		
+		int maxDay = getDays(y, m);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+		
+		// print first line
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+
+		// print from second line to last
+
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim)
+				System.out.println();
+		}
+
+		System.out.printf("\n");
+	}
+	
+	private int getWeekDay(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 3; //1970/Jan/1st = Thursday
+		
+		int count = 0;
+		
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+		
+		//System.out.println(count);
+		for (int i = 1; i < month; i++) {
+			int delta = getDays(year, i);
+			count += delta;
+		}
+		
+		count += day;
+		
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+		return weekday;
 	}
 }
--- src/calender/Month.java
+++ src/calender/Month.java
@@ -4,7 +4,21 @@ import java.util.Scanner;
 
 public class Month {
 
-	private final static String PROMPT = "> "; 
+//	private final static String PROMPT = "> ";
+	private static final String[] weekday = new String[] {"SU", "MO", "TU", "WE", "TH", "FR", "SA"};
+	
+	public int parseDay(String w)
+	{
+		if(w.equals("SU")) return 0;
+		else if(w.equals("MO")) return 1;
+		else if(w.equals("TU")) return 2;
+		else if(w.equals("WE")) return 3;
+		else if(w.equals("TH")) return 4;
+		else if(w.equals("FR")) return 5;
+		else if(w.equals("SA")) return 6;
+		else return 0;
+	}
+	
 	public void runPrompt() 
 	{
 		Scanner scanner = new Scanner(System.in);
@@ -12,19 +26,22 @@ public class Month {
 		
 		while(true)
 		{
+			System.out.println("해를 입력하세요.");
+			System.out.print("YEAR> ");
+			int year = scanner.nextInt();
+			
 			System.out.println("월을 입력하세요.");
-			System.out.print(PROMPT);
+			System.out.print("MONTH> ");
 			int mon = scanner.nextInt();
 			if(mon==-1) break;
 	
-			int days = cal.getDays(mon);
-			System.out.printf("%d월은 %d일까지 있습니다.\n", mon, days);
+//			System.out.println("첫번째 요일을 입력하세요. SU MO TU WE TH FR SA");
+//			System.out.print("WEEKDAY> ");
+//			String str = scanner.next();
+//			int wee = parseDay(str);
 			
-			cal.printCalendar(mon);
-		
-			
-
-			
+//			cal.printCalendar(year, mon, wee);
+			cal.printCalendar(year, mon);
 		}
 		
 		scanner.close();
